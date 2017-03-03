e7e0f0c929a06997fa70920b00e57999bbbaedb8
ahmeds24 <sheikh.ahmed206@gmail.com>
2017-03-02 20:17:10 -0800

    new function

---
+++ inputArrayGreaterThanY.js
@@ -0,0 +1,16 @@
+function returnArray(inputArray,input){
+   var count = 0;
+  for(i=o; i< returnArray(x).length; i++){
+  if(inputArray[i] > input){
+    count++;
+	}
+  }
+  console.log("There were" , count + "values greater than" input + " in the array!");
+  var test = [21,3,5,7,9,11,44,45];
+  var y = 7;
+  
+ }
+ 
+ returnArray(test,y);
+  
+  
