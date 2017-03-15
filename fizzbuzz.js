48afa36d55251d7f6b616f81e954b2a4503b8536
ahmeds24 <sheikh.ahmed206@gmail.com>
2017-03-14 18:36:02 -0700

    adding more challenges

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+var number = 0;
+function fizzBuzx(x){
+for(i=1; i<=100; i++){
+   if(i % 15){
+   console.log("fizzbuzz");
+   }
+   else if(i % 3){
+   console.log("fizz");
+   }
+   else if (i % 5){
+   console.log("buzz");
+   }
+   else{
+	   console.log(i);
+   }
+   
+  }
+  
+}
+ 
+ 
+ 
+ fizzbuzz();
